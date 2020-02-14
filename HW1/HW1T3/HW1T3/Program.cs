8f4e451b91c12e1723a6603d3751aab482019389
mihail.beloshapkin <misha.beloshapkin@yandex.ru>
2020-02-14 23:19:18 +0300

    sort

---
+++ HW1/HW1T3/HW1T3/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace HW1T3
+{
+    class Program
+    {
+        private static void CountSort(int[] array)
+        {
+            int maxNumber = array[0];
+            int minNumber = array[0];
+
+            for (int index = 0; index < array.Length; index++)
+            {
+                if (array[index] > maxNumber)
+                {
+                    maxNumber = array[index];
+                }
+                if (array[index] < minNumber)
+                {
+                    minNumber = array[index];
+                }
+            }
+            int[] countArray = new int[maxNumber - minNumber + 1];
+            for (int index = 0; index < array.Length; index++)
+            {
+                countArray[array[index] - minNumber]++;
+            }
+
+            int indexOfOutputArray = 0;
+
+            for (int index = 0; index < maxNumber - minNumber + 1; index++)
+            {
+                while (countArray[index] > 0)
+                {
+                    array[indexOfOutputArray] = index + 1;
+                    indexOfOutputArray++;
+                    countArray[index]--;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input size of array:");
+            var input = Console.ReadLine();
+            int sizeOfArray = int.Parse(input);
+            if (sizeOfArray <= 0)
+            {
+                Console.WriteLine("Incorrect value of array size");
+                return;
+            }
+            int[] array = new int[sizeOfArray];
+            for (int index = 0; index < sizeOfArray; index++)
+            {
+                var inputValue = Console.ReadLine();
+                array[index] = int.Parse(inputValue);
+            }
+            CountSort(array);
+            Console.WriteLine("Sorted array:");
+            for (int index = 0; index < array.Length; index++)
+            {
+                Console.WriteLine(array[index]);
+            }
+        }
+    }
+}
