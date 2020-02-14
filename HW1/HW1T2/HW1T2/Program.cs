fa130143acd872f33547cb5c145e5f96a9a6e2c2
mihail.beloshapkin <misha.beloshapkin@yandex.ru>
2020-02-14 20:36:08 +0300

    fibanacci

---
+++ HW1/HW1T2/HW1T2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HW1T2
+{
+    class Program
+    {
+        private static int recursiveCalculation(int value)
+        {
+            if (value <= 1)
+            {
+                return value;
+            }
+            else
+            {
+                return recursiveCalculation(value - 1) + recursiveCalculation(value - 2);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input value:");
+            var inputString = Console.ReadLine();
+            int value = int.Parse(inputString);
+            Console.WriteLine("Result of calculation is: ");
+            Console.WriteLine(recursiveCalculation(value));
+        }
+    }
+}
