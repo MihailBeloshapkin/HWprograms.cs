c234eaa9a120705373b0678b3df0f23d01e6ba78
mihail.beloshapkin <misha.beloshapkin@yandex.ru>
2020-02-15 18:12:17 +0300

    fix

--- HW1/HW1T2/HW1T2/Program.cs
+++ HW1/HW1T2/HW1T2/Program.cs
@@ -4,24 +4,24 @@ namespace HW1T2
 {
     class Program
     {
-        private static int RecursiveCalculation(int value)
+        private static int IterCalculation(int value)
         {
-            if (value <= 1)
+            int[] fibanacciArray = new int[3] { 1, 1, 0 };
+            for (int iter = 0; iter < value - 2; iter++)
             {
-                return value;
+                fibanacciArray[2] = fibanacciArray[0] + fibanacciArray[1];
+                fibanacciArray[0] = fibanacciArray[1];
+                fibanacciArray[1] = fibanacciArray[2];
             }
-            else
-            {
-                return RecursiveCalculation(value - 1) + RecursiveCalculation(value - 2);
-            }
+            return fibanacciArray[2];
         }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Input value:");
             var inputString = Console.ReadLine();
             int value = int.Parse(inputString);
-            Console.WriteLine("Result of calculation is: ");
-            Console.WriteLine(RecursiveCalculation(value));
+            Console.WriteLine($"Result of calculation is: {IterCalculation(value)}");
         }
     }
 }
