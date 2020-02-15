997b9c97070d4e6f40a0803deb859222634b9a0a
mihail.beloshapkin <misha.beloshapkin@yandex.ru>
2020-02-15 13:24:20 +0300

    spiral

---
+++ HW1/HW1T4/HW1T4/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace HW1T4
+{
+    class Program
+    {
+        private static void DisplaySpiral(int[,] matrix)
+        {
+            int index1 = matrix.GetLength(0) / 2;
+            int index2 = matrix.GetLength(1) / 2;
+            int step = 1;
+            while (index1 != matrix.GetLength(0) - 1)
+            {
+                for (int i = 0; i < step; i++)
+                {
+                    Console.WriteLine(matrix[index1, index2]);
+                    index1--;
+                }
+                for (int i = 0; i < step; i++)
+                {
+                    Console.WriteLine(matrix[index1, index2]);
+                    index2--;
+                }
+                step++;
+                for (int i = 0; i < step; i++)
+                {
+                    Console.WriteLine(matrix[index1, index2]);
+                    index1++;
+                }
+                for (int i = 0; i < step;  i++)
+                {
+                    Console.WriteLine(matrix[index1, index2]);
+                    index2++;
+                }
+                step++;
+            }
+            for (int i = 0; i < step; i++)
+            {
+                Console.WriteLine(matrix[index1, index2]);
+                index1--;
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[5, 5] { {13, 12, 11, 10, 25 }, 
+                                            {14, 3, 2, 9, 24 }, 
+                                            {15, 4, 1, 8, 23 }, 
+                                            {16, 5, 6, 7, 22 }, 
+                                            {17, 18, 19, 20, 21 } };
+            DisplaySpiral(matrix);
+        }
+    }
+}
