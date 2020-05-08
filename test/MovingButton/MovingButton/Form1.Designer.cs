e088ae4410ca2e1d6faccd127d96f5125e3f28c2
mihail.beloshapkin <misha.beloshapkin@yandex.ru>
2020-05-08 17:00:18 +0300

    add comments

--- test/MovingButton/MovingButton/Form1.Designer.cs
+++ test/MovingButton/MovingButton/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MovingButton
 {
-    partial class Form1
+    partial class MovingButton
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(123, 106);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(51, 41);
             this.button1.TabIndex = 0;
@@ -46,7 +46,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(304, 255);
+            this.ClientSize = new System.Drawing.Size(295, 271);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
--- test/MovingButton/MovingButton/Form1.cs
+++ test/MovingButton/MovingButton/Form1.cs
@@ -10,14 +10,53 @@ using System.Windows.Forms;
 
 namespace MovingButton
 {
-    public partial class Form1 : Form
+    /// <summary>
+    /// Moving button form.
+    /// </summary>
+    public partial class MovingButton : Form
     {
-        public Form1()
+        public MovingButton()
         {
             InitializeComponent();
         }
 
         Random rnd = new Random();
+
+        private int Mod(int value)
+        {
+            if (value < 0)
+            {
+                return -value;
+            }
+            return value;
+        }
+
+        /// <summary>
+        /// Random sign
+        /// </summary>
+        /// <returns>1 or -1</returns>
+        private int RandomSign()
+        {
+            return rnd.Next() > 0 ? 1 : -1;
+        }
+
+        /// <summary>
+        /// Change onr coordinate of the point.
+        /// </summary>
+        /// <param name="coordinate"></param>
+        /// <returns></returns>
+        private int ChangeCoordinates(int coordinate)
+        {
+            if (coordinate < 60)
+            {
+                return coordinate + 70 + Mod(rnd.Next() % 30);
+            }
+            if (coordinate > 170)
+            {
+                return coordinate - 70 - rnd.Next() % 30;
+            }
+            return coordinate + RandomSign() * (30 + Mod(rnd.Next()) % 50);
+        }
 
         /// <summary>
         /// Move the  button when hovering.
@@ -26,7 +65,7 @@ namespace MovingButton
         /// <param name="e"></param>
         private void button1_MouseHover(object sender, EventArgs e)
         {
-            button1.Location = new Point(40 + rnd.Next() % 160, 40 + rnd.Next() % 160);
+            button1.Location = new Point(ChangeCoordinates(button1.Location.X), ChangeCoordinates(button1.Location.Y));
         }
 
         /// <summary>
