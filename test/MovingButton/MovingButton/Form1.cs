c5bebd59ce8f231e27a613f10653aeb683976d1e
mihail.beloshapkin <misha.beloshapkin@yandex.ru>
2020-05-08 16:28:31 +0300

    add new test solution

---
+++ test/MovingButton/MovingButton/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovingButton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rnd = new Random();
+
+        /// <summary>
+        /// Move the  button when hovering.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+            button1.Location = new Point(40 + rnd.Next() % 160, 40 + rnd.Next() % 160);
+        }
+
+        /// <summary>
+        /// Show the message box and exit from the application.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Game Over");
+            Application.Exit();
+        }
+    }
+}
