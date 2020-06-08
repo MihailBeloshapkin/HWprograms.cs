953a6303c171360ce229ffb5609e47d7b3103aad
mihail.beloshapkin <misha.beloshapkin@yandex.ru>
2020-06-08 14:33:00 +0300

    add button and progress bar

---
+++ test/test1v3/test1v3/ButtonAndProgressBar.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace test1v3
+{
+    partial class ButtonAndProgressBar
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.IndicatorButton = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // IndicatorButton
+            // 
+            this.IndicatorButton.Location = new System.Drawing.Point(294, 73);
+            this.IndicatorButton.Name = "IndicatorButton";
+            this.IndicatorButton.Size = new System.Drawing.Size(146, 72);
+            this.IndicatorButton.TabIndex = 0;
+            this.IndicatorButton.Text = "plain terrible button";
+            this.IndicatorButton.UseVisualStyleBackColor = true;
+            this.IndicatorButton.Click += new System.EventHandler(this.IndicatorButton_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(229, 178);
+            this.progressBar.MarqueeAnimationSpeed = 50;
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(266, 83);
+            this.progressBar.Step = 1;
+            this.progressBar.TabIndex = 1;
+            this.progressBar.Click += new System.EventHandler(this.progressBar_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 700;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Location = new System.Drawing.Point(267, 317);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(194, 73);
+            this.CloseButton.TabIndex = 2;
+            this.CloseButton.Text = "Close this program";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Visible = false;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // ButtonAndProgressBar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.IndicatorButton);
+            this.Name = "ButtonAndProgressBar";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button IndicatorButton;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button CloseButton;
+    }
+}
+
