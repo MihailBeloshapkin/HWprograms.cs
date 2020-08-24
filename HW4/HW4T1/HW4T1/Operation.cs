71760a6488c70a49c1bcb8f5b5f0bb1325fe8cd7
MihailBeloshapkin <misha.beloshapkin@yandex.ru>
2020-08-24 15:08:19 +0300

    fix comment

--- HW4/HW4T1/HW4T1/Operation.cs
+++ HW4/HW4T1/HW4T1/Operation.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace HW4T1
 {
+    /// <summary>
+    /// This class contains operation methods used in the parse tree.
+    /// </summary>
     public abstract class Operation : ITreeElement
     {
         public ITreeElement left { get; set; }
